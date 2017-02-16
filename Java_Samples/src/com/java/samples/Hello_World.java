32a859040dd556930f50c5fb958ae519b58a3458
balavamsi <kodativamsi@gmail.com>
2017-02-15 23:14:23 -0700

    Hello World

---
+++ Java_Samples/src/com/java/samples/Hello_World.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.java.samples;
+
+
+/**
+ * @author balavamsi
+ *
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World... Welcome to Java");
+
+	}
+
+}
